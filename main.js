f4d8863063dd9e839cbd4d774be7c0dce5373e27
Gavrila-hub <61890882+Gavrila-hub@users.noreply.github.com>
2020-03-12 00:24:49 +0000

    update main.js
    changed the form and created validation

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 //create a counter that will be used to display the articles
+document.getElementById("formContainer").style.display = "none";
 var counter = 1;
 var articleContainer = document.getElementById("articleContainer");
 var btnShowArticle = document.getElementById("btnShowArticle");
 var title = document.getElementById("title");
 var visited = false;
+
 //create an event listener for the button 
 btnShowArticle.addEventListener("click", function(){
 	
@@ -19,15 +21,22 @@ btnShowArticle.addEventListener("click", function(){
 	};	
 	fileRequest.send(); 
 	
+	//check if the counter if less than 5
 	if( counter < 5 )
 		//increment the counter so that the next article is being accessed
 		counter++;
+		
 	else
 	{
+		//this means the user have see the five articles
 		visited = true;	
+		//change the counter back to one
 		counter = 1;
 	}
 });
+
+
+
 //create a function that will display the data from the file
 function renderHTML(data){
 	//create a variable that will store the data from the file
@@ -74,33 +83,52 @@ function renderHTML(data){
 	}
 	
 	if(visited == true){
-			htmlString += "<table>" +
-			"<form action='index.html' method=POST>" +
-			"<tr> <th>Article</th> <th>Ranking</th> </tr>" +
-			"<tr> <td> <label for='article1'> Article 1 </label> </td>"+
-			"<td><input type='number' id='article1' name='article1' min=1 max=5></td> </tr>" +
-			
-			"<tr> <td> <label for='article2'> Article 2 </label> </td>"+
-			"<td><input type='number' id='article2' name='article2' min=1 max=5></td> </tr>" +
-			
-			"<tr> <td> <label for='article3'> Article 3 </label> </td>"+
-			"<td><input type='number' id='article3' name='article3' min=1 max=5></td> </tr>" +
-			
-			"<tr> <td> <label for='article4'> Article 4 </label> </td>"+
-			"<td><input type='number' id='article4' name='article4' min=1 max=5></td> </tr>" +
-			
-			"<tr> <td> <label for='article5'> Article 5 </label> </td>"+
-			"<td><input type='number' id='article5' name='article5' min=1 max=5></td> </tr>" +
-
-			""
-			"</form> </table>";
-		}
-	
-	//for(i=0; i < data.length; i++){
-		//htmlString += "<p>" + data[i].type + " is a " + ". </p>";
+		document.getElementById("formContainer").style.display = "block";
+		/* htmlString += "<table>" +
+		"<form name='RankForm'>" +
+		"<tr> <th>Article</th> <th>Ranking</th> </tr>" +
+		"<tr> <td> <label for='article1'> Article 1 </label> </td>"+
+		"<td><input type='number' id='article1' name='article1' min=1 max=5 required></td> </tr>" +
 		
-	//};
+		"<tr> <td> <label for='article2'> Article 2 </label> </td>"+
+		"<td><input type='number' id='article2' name='article2' min=1 max=5 required></td> </tr>" +
+		
+		"<tr> <td> <label for='article3'> Article 3 </label> </td>"+
+		"<td><input type='number' id='article3' name='article3' min=1 max=5 required></td> </tr>" +
+		
+		"<tr> <td> <label for='article4'> Article 4 </label> </td>"+
+		"<td><input type='number' id='article4' name='article4' min=1 max=5 required></td> </tr>" +
+		
+		"<tr> <td> <label for='article5'> Article 5 </label> </td>"+
+		"<td><input type='number' id='article5' name='article5' min=1 max=5 required></td> </tr>" +
+
+		"<tr> <td> <input type='submit' value='Submit'> </td> </tr> " +
+		"</form> </table>"; */
+
+	}
 	
+
 	articleContainer.innerHTML = "";
 	articleContainer.insertAdjacentHTML('beforeend', htmlString);
-};
+
+	
+}
+var rankForm = document.getElementById('rankForm');
+    rankForm.addEventListener('submit', function(){
+		var article1 = document.getElementById('article1');
+		var article2 = document.getElementById('article2');
+		var article3 = document.getElementById('article3');
+		var article4 = document.getElementById('article4');
+		var article5 = document.getElementById('article5');
+		if(article1.value == article2.value){
+			alert("the values are the same!");
+			return false;
+		}
+		else{
+			alert("the infomation was sent")
+			return true
+			
+		}
+	});
+
+	
